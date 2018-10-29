b585632ffe9c753f6b52b180b69374e8b02d5679
Ramiro Berrelleza <rberrelleza@gmail.com>
2018-10-29 15:26:53 -0700

    0.1.0 release

---
+++ Formula/okteto.rb
@@ -0,0 +1,17 @@
+class Okteto < Formula
+    desc "CLI for cloud native development"
+    homepage "https://okteto.com"
+    url "https://s3-us-west-1.amazonaws.com/okteto-cli/0.1.0/okteto-darwin-amd64"
+    sha256 "d17295f0ef88f3d0e6792a48cfd1bf48a74e9d5f60fae2d7ca311ec55c8a59a8"
+    version "0.1.0"
+
+    def install
+        bin.install "okteto-darwin-amd64"
+        mv bin/"okteto-darwin-amd64", bin/"okteto"
+    end
+
+    # Homebrew requires tests.
+    test do
+        shell_output("#{bin}/okteto", 0)
+    end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Okteto
+Brew tap for the [okteto](https://okteto.com) cli
+
+## Usage
+```
+brew tap okteto/cli
+brew install okteto
+```
