57d0a83a6580ed25f53005578f6f9c922e9246d0
okteto <ci@okteto.com>
2018-10-31 11:43:23 +0000

    0.2.0 release

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,9 +1,9 @@
 class Okteto < Formula
     desc "CLI for cloud native development"
     homepage "https://okteto.com"
-    url "https://s3-us-west-1.amazonaws.com/okteto-cli/0.1.2/okteto-darwin-amd64"
-    sha256 "5f7f111baff6d1c769751e21a140a0b82f350d9cf9b6d10e8d426864b836f569"
-    version "0.1.2"
+    url "https://s3-us-west-1.amazonaws.com/okteto-cli/0.2.0/okteto-darwin-amd64"
+    sha256 "cb23b0027ec50f67f7664f682d8b8efd2c249de733108f891663e14b6d69018c"
+    version "0.2.0"
     depends_on "syncthing"
 
     def install
@@ -13,6 +13,6 @@ class Okteto < Formula
 
     # Homebrew requires tests.
     test do
-        assert_match "okteto version 0.1.2", shell_output("#{bin}/okteto version 2>&1", 0)
+        assert_match "okteto version 0.2.0", shell_output("#{bin}/okteto version 2>&1", 0)
     end
 end
