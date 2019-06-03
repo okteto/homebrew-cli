0effb5b49267d6d8229363e5c15972c9ca14c5ab
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-06-03 11:42:25 -0700

    Manually update okteto to 1.0.8

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,21 +1,21 @@
 class Okteto < Formula
     desc "CLI for cloud native development"
     homepage "https://okteto.com"
-    url "https://downloads.okteto.com/cli/0.7.3/okteto-darwin-amd64"
-    sha256 "43f58c453d4dc35fbc97d0b19830a3916fcdc8a2005cc601d3da566495ffa1ee"
-    version "0.7.3"
+    url "https://downloads.okteto.com/cli/okteto-Darwin-x86_64"
+    sha256 "6588979ffd31a399cbdfd7e71c0c24a9018477e5b66a36270452574a5f38e93a"
+    version "1.0.8"
     
     devel do
-        url "https://downloads.okteto.com/cli/master/okteto-darwin-amd64"
+        url "https://downloads.okteto.com/cli/master/okteto-Darwin-x86_64"
     end
     
     def install
-        bin.install "okteto-darwin-amd64"
-        mv bin/"okteto-darwin-amd64", bin/"okteto"
+        bin.install "okteto-Darwin-x86_64"
+        mv bin/"okteto-Darwin-x86_64", bin/"okteto"
     end
 
     # Homebrew requires tests.
     test do
-        assert_match "okteto version 0.7.3", shell_output("#{bin}/okteto version 2>&1", 0)
+        assert_match "okteto version 1.0.8", shell_output("#{bin}/okteto version 2>&1", 0)
     end
 end
