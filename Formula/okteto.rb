8e2af25a2823251dfd7eb279910a46119116f733
okteto <ci@okteto.com>
2019-09-06 18:29:58 +0000

    1.4.4 release

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -2,8 +2,8 @@ class Okteto < Formula
     desc "CLI for cloud native development"
     homepage "https://okteto.com"
     url "https://downloads.okteto.com/cli/okteto-Darwin-x86_64"
-    sha256 "36b0950e096aa5b53c8569228fc8e6911bb314823fe288b3c01d4c30cb4b3c05"
-    version "1.4.3"
+    sha256 "a3978a48d28669eda1fe1850de3ffeac7ed64a777cc947f0e071d83adfe816b4"
+    version "1.4.4"
     
     devel do
         url "https://downloads.okteto.com/cli/master/okteto-Darwin-x86_64"
@@ -16,6 +16,6 @@ class Okteto < Formula
 
     # Homebrew requires tests.
     test do
-        assert_match "okteto version 1.4.3", shell_output("#{bin}/okteto version 2>&1", 0)
+        assert_match "okteto version 1.4.4", shell_output("#{bin}/okteto version 2>&1", 0)
     end
 end
